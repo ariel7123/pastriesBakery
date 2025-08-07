810ac13c4d788d1103484ff10f3b07d85f42c34f
yatirye <yatirye@gmail.com>
2025-08-07 18:47:39 +0300

    making class

---
+++ scripts/dist/index.js
@@ -0,0 +1,18 @@
+var product = /** @class */ (function () {
+    function product(productName, productImage, productPrice, inStock) {
+        this.productName = productName;
+        this.productImage = productImage;
+        this.productPrice = productPrice;
+        this.inStock = inStock;
+    }
+    return product;
+}());
+var products = [
+    new product("mini cockies", new URL("./images/DP-05430.webp"), 150, true),
+    new product("donats", new URL("./images/dp-05501.webp"), 120, true),
+    new product("flavor mafins", new URL("./images/dp-07298_1024x683.webp"), 200, true),
+    new product("cream buns", new URL("./images/dp-07499_1024x683.webp"), 180, true),
+    new product("creamy cockies", new URL("./images/dp-07512.webp"), 125, true),
+    new product("sweet mix", new URL("./images/dp-07564_1024x682.webp"), 250, true),
+    new product("the backlawa", new URL("./images/DP-09542.webp"), 300, true),
+];
